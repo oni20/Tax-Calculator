e29e33935dfa230f77f4f3445b27fb03bfb69951
Oni20 <oniahmad20@gmail.com>
2021-02-18 13:24:40 -0500

    Moved Errorboundary class to Component folder

--- tax-calculator-next/pages/_app.js
+++ tax-calculator-next/pages/_app.js
@@ -1,4 +1,4 @@
-import ErrorBoundary from './ErrorBoundary';
+import ErrorBoundary from '../components/ErrorBoundary';
 import GlobalContextProvider from '../components/Context/GlobalContext'
 
 /* Import styling */
